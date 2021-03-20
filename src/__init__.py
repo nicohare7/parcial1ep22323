14391ef4a71a6a086d6716a99ca47036c288e8ca
nicolas07 <79719980+nicohare7@users.noreply.github.com>
2021-03-19 23:45:24 -0500

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, template_folder= 'views')
-
-#importar los controllers
-from src.controllers import *
-
-def create_app():
-    app.run(debug=True, port=5000)
--- src/controllers/home.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from src import app
-
-@app.route('/')
-def index():
-    return render_template('/usuarios/index.html')
